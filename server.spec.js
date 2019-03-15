d8db0506de3b82cda7dd16d29da34d4a35d2e5c2
Estevan <estevan.rodriguez10@yahoo.com>
2019-03-15 09:19:37 -0700

    finished testing GET endpoint and converted it statements into async

--- server.spec.js
+++ server.spec.js
@@ -5,14 +5,33 @@ describe('server.js', () => {
     
     describe('Test Get /api/games', () => {
         
-        it('should return a status of 200', () => {
+        it('should return a status of 200', async () => {
             const res = request(server).get('/api/games');
             expect(res.status).toBe(200);
         });
 
-        it('should return an empty array when there is no data', () => {
-            const res = request.server.get('/api/games');
+        it('should return an empty array when there is no data', async () => {
+            const res = request.server.get('/api/games')
             expect(res.body).toEqual([]);
+        })
+        
+        it('should return data provided', async () => {
+            const res = await request(server).get('/api/games')
+            expect(res.body).toEqual([
+                {
+                    id:0,
+                    title: 'Pacman', // required
+                    genre: 'Arcade', // required
+                    releaseYear: 1980 // not required
+                },
+                {
+                    id:1,
+                    title: 'Mario', // required
+                    genre: 'action', // required
+                    releaseYear: 1982  
+                },
+            
+            ])
         })
     });
 });
