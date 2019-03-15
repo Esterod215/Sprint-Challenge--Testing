0bc285118be37c806ef49a6979f7fcf536303d49
Estevan <estevan.rodriguez10@yahoo.com>
2019-03-15 09:01:25 -0700

    wrote one test for get to check to see if status code of 200 was returned.

--- server.spec.js
+++ server.spec.js
@@ -2,5 +2,12 @@ const request = require('supertest');
 const server = require('./server.js');
 
 describe('server.js', () => {
-    describe('Test Get /api/games')
-})
+    
+    describe('Test Get /api/games', () => {
+        
+        it('should return a status of 200', () => {
+            const res = request(server).get('/api/games');
+            expect(res.status).toBe(200);
+        });
+    });
+});
