2a3ec34e3fe2769bef85de2e0aa18ba1c174e1ac
Estevan <estevan.rodriguez10@yahoo.com>
2019-03-15 08:52:04 -0700

    finished writing enpoints and will now begin to test

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+const games = [
+    {
+        id:0,
+        title: 'Pacman', // required
+        genre: 'Arcade', // required
+        releaseYear: 1980 // not required
+    },
+    {
+        id:1,
+        title: 'Mario', // required
+        genre: 'action', // required
+        releaseYear: 1982  
+    },
+
+];
+
+server.get('/api/games',(req,res)=>{
+    res.status(200).json(games);
+});
+
+server.post('/api/games',(req,res)=>{
+    const { id, title, genre } = req.body;
+    
+    if(!id || !title || !genre){
+        res.status(422).json('Incomplete fields');
+    }
+    
+    res.status(201).json([games]);
+})
+
+server.listen('localhost:3000',()=>{
+    console.log('server listening on port 3000');
+});
+
+
+
+
+module.exports = server;
